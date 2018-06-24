f05ea84d794f45f7b47493aac37048313b62b2d1
brodieberger <40439538+brodieberger@users.noreply.github.com>
2018-06-24 17:06:53 -0400

    Update (A) Sequence with a fixed number or loops.py

--- (A) Sequence with a fixed number or loops.py
+++ (A) Sequence with a fixed number or loops.py
@@ -1,6 +1,9 @@
 x = int(input("Enter the maximum number: "))
+#sets x as the number it will take to break the loop
 counter = 1
+#the counter will begin at one 
 while counter < x+1:
     print(counter)
     counter = counter + 1
+#The counter will have one added to it until it reaches the set value for x, the maximum number
 input('Press ENTER to exit')
