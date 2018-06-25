dbbcf6f7bd528bd9ac0d7d9ae46e1296caf64318
brodieberger <40439538+brodieberger@users.noreply.github.com>
2018-06-24 21:14:47 -0400

    Update (D) Entry control loop that halts.py

--- (D) Entry control loop that halts.py
+++ (D) Entry control loop that halts.py
@@ -1,8 +1,10 @@
 x = int(input("Select starting number: "))
 highest = int(input("Select number that will break the loop: "))
+#sets the starting number and the number that will break the loop
 while x < highest:
     print(x)
     x = x + 1
 else:
     print("Starting number has reached", highest, "terminating loop")
+#adds one to the starting number until it reaches the specified number that breaks the loop
 input("press enter to exit")
