61a04ed113c5775a50513eeaaa5af38668bff1a1
brodieberger <40439538+brodieberger@users.noreply.github.com>
2018-06-24 21:17:17 -0400

    Update (F) Condition controlled loops.py

--- (F) Condition controlled loops.py
+++ (F) Condition controlled loops.py
@@ -3,13 +3,16 @@ count = int(input("Set starting number (1-10): "))
 if count > 10:
     input("Invalid input")
     sys.exit()
+#condition that will terminate the program if not met. (1-10)
     
 highest = int(input("Set number to break loop (1-100): "))
 if highest > 100:
     input("Invalid input")
     sys.exit()
+#condition that will terminate the program if not met. (1-100)
     
 while count < highest:
 	print(count) 
 	count +=1
+#adds one to the count until it reaches the specified highest number
 input ("Press Enter to Exit")
