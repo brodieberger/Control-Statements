e3e2ed71f805f8debdadafb252ace59ae53ba985
brodieberger <40439538+brodieberger@users.noreply.github.com>
2018-06-24 21:30:04 -0400

    Update (G) Compound boolean expressions.py

--- (G) Compound boolean expressions.py
+++ (G) Compound boolean expressions.py
@@ -1,14 +1,16 @@
+#Boolean expressions are statements that equal true or false. Here are examples using (and) and (or) expressions.
+
 print ("if 1 < 2 and 4 > 2:")
 if 1 < 2 and 4 > 2:
     print("condition met \n")
-
 if 1 > 2 and 4 < 10:
     print("condition not met \n")
+#This says if both of these are true then it will meet the conditions. If one is false then the answer will remain false.
 
 print("if 4 < 10 or 1 < 2: ")
 if 4 < 10 or 1 < 2:
     print("condition met \n")
 else:
     print("condition not met")
-    
+#This says if either of the equations are true the conditions will be met. If one is true and the other is false, the answer will be true.
 input ("Press Enter to Exit")
