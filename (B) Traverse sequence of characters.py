f6e498feaac6037099295eb118c82d2509a3a6fc
brodieberger <40439538+brodieberger@users.noreply.github.com>
2018-06-24 20:53:49 -0400

    Update (B) Traverse sequence of characters.py

--- (B) Traverse sequence of characters.py
+++ (B) Traverse sequence of characters.py
@@ -1,4 +1,6 @@
 x = str(input("Enter a string: "))
+#enters a word to traverse
 for char in x:
     print (char)
+#print each letter on a separate line, then breaks when out of letters
 input('Press ENTER to exit')
