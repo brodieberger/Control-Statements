60c6a6e563fdd18e95f81f1c33344099d5145317
brodieberger <40439538+brodieberger@users.noreply.github.com>
2018-06-24 20:54:52 -0400

    Update (C) Loop that counts up and down.py

--- (C) Loop that counts up and down.py
+++ (C) Loop that counts up and down.py
@@ -1,9 +1,12 @@
 count = 100
+#starts the count at 100
 while count > 0 :
     print(count)
     count = count - 1
+#while the count number is greater than 1, subtract one then repeat
 count = 1
 while count <= 100:
     print(count)
     count += 1
+#while the count number is less than 100, add one then repeat
 input("press enter to exit")
